755a22ddf4dd4ce0bbf25845139b7b4328f0b5dc
Matthew Bourque <bourque@stsci.edu>
2020-07-29 15:48:15 -0400

    Some commands changed due to new project name

--- exo_bespin/atmospheric_retrievals/build-exo_bespin-env-cpu.sh
+++ exo_bespin/atmospheric_retrievals/build-exo_bespin-env-cpu.sh
@@ -11,33 +11,31 @@ echo '~~~~~ SETTING ENVIRONMENT VARIABLES ~~~~~'
 echo ''
 export PATH=/home/ec2-user/miniconda3/bin:$PATH
 echo 'Set $PATH'
-export EXOCTK_DATA=''
-echo 'Set $EXOCTK_DATA'
 
 echo ''
 echo '~~~~~ CREATING base CONDA ENVIRONMENT ~~~~~'
 echo ''
-conda create --yes -n exoctk-3.6 python=3.6 git numpy flask pytest
+conda create --yes -n exo-bespin python=3.7 git numpy flask pytest
 conda init bash
 source ~/.bashrc
-conda activate exoctk-3.6
+conda activate exo-bespin
 
 echo ''
 echo '~~~~~ INSTALLING exoctk ~~~~~'
 echo ''
-git clone https://github.com/ExoCTK/exoctk.git
-cd exoctk/
-conda env update -f env/environment-3.6.yml
+git clone https://github.com/exo-bespin/exo_bespin.git
+cd exo_bespin/
+conda env update -f env/exo-bespin
 conda init bash
 source ~/.bashrc
-conda activate exoctk-3.6
+conda activate exo-bespin
 python setup.py develop
 cd ../
 
 echo ''
 echo '~~~~~ INSTALLING jwst_gtvt ~~~~~'
 echo ''
-rm -fr /home/ec2-user/miniconda3/envs/exoctk-3.6/lib/python3.6/site-packages/jwst_gtvt
+rm -fr /home/ec2-user/miniconda3/envs/exo-bespin/lib/python3.7/site-packages/jwst_gtvt
 git clone https://github.com/spacetelescope/jwst_gtvt.git
 cd jwst_gtvt
 git checkout cd6bc76f66f478eafbcc71834d3e735c73e03ed5
@@ -51,17 +49,6 @@ pip install bibtexparser==1.1.0
 pip install corner==2.0.1
 pip install lmfit==0.9.13
 pip install platon==3.1
-
-echo ''
-echo '~~~~~ BUILDING EXOCTK_DATA DIRECTORY ~~~~~'
-echo ''
-mkdir exoctk_data/
-mkdir exoctk_data/exoctk_contam/
-mkdir exoctk_data/exoctk_log/
-mkdir exoctk_data/fortney/
-mkdir exoctk_data/generic/
-mkdir exoctk_data/groups_integrations/
-mkdir exoctk_data/modelgrid/
 
 echo ''
 echo '~~~~~ THE ENVIRONMENT BEING USED ~~~~~'
