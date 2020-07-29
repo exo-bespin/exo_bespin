ceca44eab63a1dcbddf2812c194565ccf64407d3
Matthew Bourque <bourque@stsci.edu>
2020-07-29 15:34:10 -0400

    Initial commit of setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+REQUIRES = ['numpy',
+            'platon']
+
+setup(name='exo_bespin',
+      version='0.0.0',
+      description='',
+      packages=find_packages(".", exclude=["*.tests"]),
+      install_requires=REQUIRES,
+      author='Matthew Bourque, Rachel Cooper, Néstor Espinoza',
+      license='BSD 3',
+      url='https://github.com/exo-bespin/exo_bespin'
+)
