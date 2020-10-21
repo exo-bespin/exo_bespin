da46d6a178e07ea8afdba6ee1d5fa6d444e56c97
Matthew Bourque <bourque@stsci.edu>
2020-10-21 10:01:41 -0400

    Initial commit -m logging_tools

---
+++ exo_bespin/logging/logging_tools.py
@@ -0,0 +1,69 @@
+"""This module contains various functions for logging the execution of
+``exo_bespin`` software
+
+Authors
+-------
+
+    - Matthew Bourque
+
+Use
+---
+
+    This script is inteneded to be imported and used by other modules,
+    for example:
+
+        from exo_bespin.logging.logging_tools import configure_logging
+        configure_logging('my_log')
+
+    This will create a log file at the location ``$HOME/my_log.log``
+
+    Users may also supply a ``log_dir`` parameter to change the parent
+    directory in which the log file is saved, for example:
+
+        configure_logging('my_log', '/user/myself/log_files/')
+
+    This will create a log file at the location
+    ``/user/myself/log_files/my_log.log``
+"""
+
+import datetime
+import logging
+import os
+
+
+def configure_logging(log_filename, log_dir=os.path.expanduser("~")):
+    """Configure the log file with a standard logging format.
+
+    Parameters
+    ----------
+    log_filename : str
+        The name that will be used to create the log file (e.g.
+        ``my_log_<timestamp>.log``)
+
+    Returns
+    -------
+    full_filename : str
+        The full path to the created log file
+    """
+
+    # Make sure log_dir exists
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    # Build complete log filename
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')
+    full_filename = os.path.join(log_dir, '{0}_{1}.log'.format(log_filename, timestamp))
+
+    # Make sure no other root lhandlers exist before configuring the logger
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+
+    # Create the log file and set the permissions
+    logging.basicConfig(filename=full_filename,
+                        format='%(asctime)s %(levelname)s: %(message)s',
+                        datefmt='%m/%d/%Y %H:%M:%S %p',
+                        level=logging.INFO)
+
+    print('Log file initialized to {}'.format(full_filename))
+
+    return full_filename
