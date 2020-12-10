054cad132da369e8ad0b8af27065eaf210061ccf
Matthew Bourque <bourque@stsci.edu>
2020-12-10 11:30:42 -0500

    Initial commit of django web app

---
+++ exo_bespin/website/bespin_app/urls.py
@@ -0,0 +1,53 @@
+"""Maps URL paths to views in the ``bespin`` app.
+
+This module connects requested URL paths to the corresponding view in
+``views.py`` for each webpage in the ``bespin`` app. When django is
+provided a path, it searches through the ``urlpatterns`` list provided
+here until it finds one that matches. It then calls the assigned view
+to load the appropriate webpage, passing an ``HttpRequest`` object.
+
+Authors
+-------
+
+    - Matthew Bourque
+
+Use
+---
+
+    Function views
+        1. Add an import:  from my_app import views
+        2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    Class-based views
+        1. Add an import:  from other_app.views import Home
+        2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    Including another URLconf
+        1. Import the include() function: from django.urls import include, path
+        2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+References
+----------
+    For more information please see:
+        ``https://docs.djangoproject.com/en/2.0/topics/http/urls/``
+
+Notes
+-----
+    Be aware that when a url is requested, it will be directed to the
+    first matching path in the ``urlpatterns`` list that it finds. The
+    ``<str:var>`` tag is just a placeholder. To avoid complications,
+    users should order their paths in order from shortest to longest,
+    and after that from most to least specific.
+"""
+
+from django.urls import path
+from django.urls import re_path
+
+from . import views
+
+#app_name = 'bespin'
+
+urlpatterns = [
+
+    # Home
+    path('', views.home, name='home'),
+
+]
---
+++ exo_bespin/website/bespin_app/views.py
@@ -0,0 +1,56 @@
+"""Defines the views for the ``bespin`` web app.
+
+In Django, "a view function, or view for short, is simply a Python
+function that takes a Web request and returns a Web response" (from
+Django documentation). This module defines all of the views that are
+used to generate the various webpages used for the ``bespin``
+application.
+
+Authors
+-------
+
+    - Matthew Bourque
+
+Use
+---
+
+    This module is called in ``urls.py`` as such:
+    ::
+
+        from django.urls import path
+        from . import views
+        urlpatterns = [path('web/path/to/view/', views.view_name,
+        name='view_name')]
+
+References
+----------
+    For more information please see:
+        ``https://docs.djangoproject.com/en/2.0/topics/http/views/``
+
+Dependencies
+------------
+
+"""
+
+from django.http import HttpRequest as request
+from django.shortcuts import render
+
+
+def home(request):
+    """Generate the home page.
+
+    Parameters
+    ----------
+    request : HttpRequest object
+        Incoming request from the webpage
+
+    Returns
+    -------
+    HttpResponse object
+        Outgoing response sent to the webpage
+    """
+
+    template = 'home.html'
+    context = {}
+
+    return render(request, template, context)
