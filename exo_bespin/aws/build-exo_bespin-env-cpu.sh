ad13abbbd893fdae1a98cb82b3c822528b9e0874
Matthew Bourque <bourque@stsci.edu>
2021-01-27 11:25:53 -0500

    Switched back to hard coding for linux until we can figure out what is wrong with using $HOME directory

--- exo_bespin/aws/build-exo_bespin-env-cpu.sh
+++ exo_bespin/aws/build-exo_bespin-env-cpu.sh
@@ -1,31 +1,36 @@
 #! /bin/bash
 
-cd $HOME  # Ensure that this script operates relative to the $HOME directory
+export HOME_DIR=/home/ec2-user/
+cd $HOME_DIR  # Ensure that this script operates relative to the $HOME_DIR directory
 
-# Additional libraries are needed, especially for C compilers
-echo ''
-echo '~~~~~ INSTALLING DEV TOOLS ~~~~~'
-echo ''
-if [ $HOME = "/home/ec2-user" ]; then
-    sudo yum -y install bzip2
-    sudo yum -y groupinstall "Development Tools"
-fi
-if [ $HOME = "/home/ubuntu" ]; then
-    sudo apt update
-    sudo apt -y install build-essential
-fi
+# This is not working since there doesn't appear to be a default $HOME directory
+# # Additional libraries are needed, especially for C compilers
+# echo ''
+# echo '~~~~~ INSTALLING DEV TOOLS ~~~~~'
+# echo ''
+# if [ $HOME = "/home/ec2-user" ]; then
+#     sudo yum -y install bzip2
+#     sudo yum -y groupinstall "Development Tools"
+# fi
+# if [ $HOME = "/home/ubuntu" ]; then
+#     sudo apt update
+#     sudo apt -y install build-essential
+# fi
+
+sudo yum -y install bzip2
+sudo yum -y groupinstall "Development Tools"
 
 echo ''
 echo '~~~~~ INSTALLING ANACONDA ~~~~~'
 echo ''
 curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh  # Download latest version of miniconda
 chmod 700 ./Miniconda3-latest-Linux-x86_64.sh  # Ensure the shell script is executable
-bash ./Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3  # Install miniconda into the $HOME directory
+bash ./Miniconda3-latest-Linux-x86_64.sh -b -p $HOME_DIR/miniconda3  # Install miniconda into the $HOME_DIR directory
 
 echo ''
 echo '~~~~~ SETTING ENVIRONMENT VARIABLES ~~~~~'
 echo ''
-export PATH=$HOME/miniconda3/bin:$PATH  # Add miniconda install path to $PATH
+export PATH=$HOME_DIR/miniconda3/bin:$PATH  # Add miniconda install path to $PATH
 
 echo ''
 echo '~~~~~ CREATING INITIAL CONDA ENVIRONMENT ~~~~~'
@@ -55,5 +60,5 @@ conda env export  #  Display full environment for logging purposes
 echo ''
 echo '~~~~~ SETTING PERMISSIONS AND COPYING INIT OUTPUT ~~~~~'
 echo ''
-chmod -R 777 $HOME/miniconda3/envs/exo-bespin/  # Open up permissions so other users can use exo-bespin environment
-cp /var/log/cloud-init-output.log $HOME  # Copy log file to home directory for easier access and to signify installation completion
+chmod -R 777 $HOME_DIR/miniconda3/envs/exo-bespin/  # Open up permissions so other users can use exo-bespin environment
+cp /var/log/cloud-init-output.log $HOME_DIR   # Copy log file to home directory for easier access and to signify installation completion
